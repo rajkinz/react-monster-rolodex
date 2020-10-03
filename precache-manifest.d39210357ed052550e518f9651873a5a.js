f41c1b32d7323137161c9018ce7b0460da613fae
Rajnish Kumar <rajkinz.kumar4@gmail.com>
2020-10-03 12:56:10 +0530

    Updates

---
+++ precache-manifest.d39210357ed052550e518f9651873a5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fbdaf6f765cab4a675a47c225946e3",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "632c541f43bca8d2c271",
+    "url": "/react-monster-rolodex/static/css/main.f25bab15.chunk.css"
+  },
+  {
+    "revision": "4324a842ba42bf058656",
+    "url": "/react-monster-rolodex/static/js/2.c12ed518.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-rolodex/static/js/2.c12ed518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632c541f43bca8d2c271",
+    "url": "/react-monster-rolodex/static/js/main.0ff14835.chunk.js"
+  },
+  {
+    "revision": "0897567ffdc70c766a48",
+    "url": "/react-monster-rolodex/static/js/runtime-main.572c3385.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
